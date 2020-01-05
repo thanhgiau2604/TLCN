5bd132b0dc7ea00d784ffd59f7b1446c5e4c2be9
thanhgiau2604 <thanhgiau2604@gmail.com>
2020-01-05 22:58:20 +0700

    final

--- test.js
+++
@@ -1,5 +0,0 @@
-var str = "       PHẦN MỘT. LỊCH SỬ THẾ GIỚI THỜI NGUYÊN THỦY, CỔ ĐẠI VÀ TRUNG ĐẠI";
-str = str.toLowerCase();
-str = str.trim();
-const newStr = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/đ/g, "d");
-console.log(newStr)
